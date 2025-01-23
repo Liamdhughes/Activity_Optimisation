0e6ad3d1b2f5699a4c4090fd1353582c90eeb7dc
Liamdhughes <146069144+Liamdhughes@users.noreply.github.com>
2025-01-23 16:54:29 +0000

    Update FL_PCA_DFA.m

--- FL_PCA_DFA.m
+++ FL_PCA_DFA.m
@@ -6,6 +6,7 @@ path(path,'D:\Liam Passport\Study 1 Data\Useful_Matlab_Functions');
 % individual, so as to train the algorithm sensor by sensor, for one
 % specific individual:
 
+
 folder_names = ['01';'02';'03';'04';'05';'06';'07';'08';'09';'10';'11';'12';'13';'14';'15';'16';'17';'18';'19';'20';'21';'22';'23';'24';'25';'26';'27';'28';'29';;'30';'31';'32';'33';'34';'35']
 for Individual_No = 1:size(folder_names,1)
     starting_times = csvread(['D:\Liam Passport\Study 1 Data\P0',folder_names(Individual_No,:),'\TDB_starting_times.csv']);
